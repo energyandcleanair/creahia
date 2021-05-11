b9c7f83bcb9374a3addcf029b41358ae58752045
laurimyllyvirta <35026118+laurimyllyvirta@users.noreply.github.com>
2021-05-11 08:03:20 +0000

    final workflow to generate one-year results for Philippines new cases

--- philippines cases workflow.R
+++ philippines cases workflow.R
@@ -1,7 +1,9 @@
+gis_dir <- "~/GIS/"
+
 devtools::install_github('energyandcleanair/creahia')
 
 library(creahia)
-creahia::set_env('gis_dir','~/GIS/')
+creahia::set_env('gis_dir',gis_dir)
 
 # For development only
 library(creahelpers)
@@ -11,13 +13,18 @@ library(readxl)
 library(zoo)
 library(magrittr)
 
-system(paste('gsutil rsync -r', get_env('gis_dir'), 'gs://crea-data/gis'))
-project_dir <- "example_ph"
-gis_dir <- "/Volumes/ext1/gis/"
+setwd(get_env('gis_dir'))
+system('gsutil rsync -r . gs://crea-data/gis')
+system('gsutil cp -r gs://crea-data/gis/* ./')
+project_dir <- "F:/TAPM/Phils/case_results"
+
 
 
 # 01: Get coal additional concentrations from CALPUFF --------------------------------------
-conc_coal_only <- get_conc_calpuff(dir=project_dir, utm_zone = 51, utm_hem = 'N', map_res =1)
+calpuff_files <- getPuffCSVs(ext=".csv", gasunit = 'ug', dir=project_dir)
+
+calpuff_files %>% filter(period=='annual' | !is.na(threshold)) %>%
+  get_conc_calpuff(utm_zone = 51, utm_hem = 'N', map_res =1) -> conc_coal_only
 
 species = unique(conc_coal_only$species)
 scenarios = unique(conc_coal_only$scenario)
@@ -25,18 +32,16 @@ grid_raster = conc_coal_only$conc_coal_only[[1]] %>% raster
 
 
 # 02: Get base concentration levels --------------------------------------------------------
-conc_base <- get_conc_base(species=species, grid_raster=grid_raster)
-
-
+conc_base <- get_conc_base(species=species, grid_raster=grid_raster, no2_targetyear = NULL)
+conc_base$conc_base$no2 %<>% multiply_by(1.2)
 # 03: Combine and flatten: one row per scenario --------------------------------------------
 concs <- combine_concs(conc_coal_only, conc_base) %>% flatten_concs() %>% add_pop()
 
 
 # 04: Create support maps (e.g. countries, provinces, cities ) -----------------------------
-adm <- get_map_adm(grid_raster, admin_level=2)
-# cities <- get_map_cities(grid_raster)
-
-adm <- adm[1:10,] # FOR DEBUG PURPOSES
+adm <- get_map_adm(grid_raster, admin_level=2, res="low")
+adm %<>% subset(country_id=='PHL') %>% head(10)
+cities <- get_map_cities(grid_raster)
 
 # 05: Extract concentrations ---------------------------------------------------------------
 conc_adm <- extract_concs_at_map(concs, adm)
@@ -62,7 +67,11 @@ gdp <- get_gdp()
 
 
 # 07: HIA Calculations: PAF -----------------------------------------------------------------
-paf <- compute_hia_paf(conc_adm, calc_causes, gemm=gemm, gbd=gbd, ihme=ihme)
+require(doParallel)
+require(future)
+plan(multisession(workers=detectCores()))
+
+paf <- compute_hia_paf(conc_adm, scenarios=scenarios, calc_causes=calc_causes, gemm=gemm, gbd=gbd, ihme=ihme)
 # saveRDS(paf, file.path(project_dir, 'paf.RDS'))
 # save.image()
 
