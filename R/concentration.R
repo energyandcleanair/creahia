ae457d9fc4e669d9b6a7b1978bc64971bfb4c114
Danny Hartono <danny.hartono@outlook.com>
2024-02-22 10:32:19 +0700

    match get_conc_baseline to wrapper.get_conc_baseline

--- R/concentration.R
+++ R/concentration.R
@@ -46,66 +46,63 @@ get_conc_calpuff <- function(calpuff_dir = NULL, calpuff_files = NULL, utm_zone,
 #' @param no2_min_incr
 #' @param no2_targetyear
 #'
-#' @return
-#' @export a tibble with `species` (chr) and `conc_baseline` (RasterLayer) columns
+#' @return a tibble with `species` (chr) and named `conc_baseline` (RasterLayer) columns
+#' @export
 #'
 #' @examples
 get_conc_baseline <- function(species,
                               grid_raster,
                               no2_min_incr = NULL,
-                              no2_targetyear = 2019) {
+                              no2_targetyear = 2019,
+                              pm25_to_pm10_ratio = .7) {
+  avail_species <- c('no2', 'so2', 'pm25', 'tpm10', 'o3') # pollutants with available baseline
+  file_list <- list(
+    'o3' = 'O3_77e3b7-xmessy_mmd_kk.nc'
+  )
 
-  conc = list()
+  species <- species[tolower(species) %in% avail_species]
 
-  if("pm25" %in% species) {
-    # source of PM2.5 data: http://fizz.phys.dal.ca/~atmos/martin/?page_id=140
-    conc[["pm25"]] <- creahelpers::get_concentration_path('GlobalGWRcwUni_PM25_GL_201601_201612-RH35_Median.nc') %>%
-      raster %>%
-      creahelpers::cropProj(grid_raster)
-  }
-
-  if("no2" %in% species) {
-    # source of NO2 data: https://data.world/datasets/no2
-    conc_no2 <- creahelpers::get_concentration_path('no2_agg8.grd') %>%
-      raster %>%
-      creahelpers::cropProj(grid_raster) %>%
-      multiply_by(1.88)
-
-    # adjust NO2 concentrations using OMI data for 2011 and the target year
-    if(!is.null(no2_targetyear)) {
-      no2_11 <- creahelpers::get_concentration_path("no2_omi_2011.tif") %>%
+  conc <- lapply(species, function(spec) {
+    if(spec == 'no2') {
+      conc_no2 <- creahelpers::get_concentration_path('no2_agg8.grd') %>%
         raster %>%
-        creahelpers::cropProj(grid_raster)
-      no2_targetyr <- creahelpers::get_concentration_path(paste0("no2_omi_", no2_targetyear, ".tif")) %>%
-        raster %>%
-        cropProj(grid_raster)
-      no2_11_smooth <- no2_11 %>%
-        focal(focalWeight(., 100, "circle"), mean, na.rm = T, pad = T, padValue = NA)
-      no2_targetyr_smooth <- no2_targetyr %>%
-        focal(focalWeight(., 100, "circle"), mean, na.rm = T, pad = T, padValue = NA)
-      no2_ratio <- no2_targetyr_smooth / no2_11_smooth
+        creahelpers::cropProj(grid_raster) %>%
+        multiply_by(1.88)
 
-      if(!is.null(no2_min_incr)) {
-        no2_ratio <- no2_ratio %>% max(no2_min_incr)
+      if(!is.null(no2_targetyear)) {
+        no2_11 <- creahelpers::get_concentration_path("no2_omi_2011.tif") %>%
+          raster %>%
+          creahelpers::cropProj(grid_raster)
+        no2_targetyr <- creahelpers::get_concentration_path(glue("no2_omi_{no2_targetyear}.tif")) %>%
+          raster %>%
+          creahelpers::cropProj(grid_raster)
+
+        no2_11_smooth <- no2_11 %>%
+          focal(focalWeight(., 100, "circle"), mean, na.rm = T, pad = T, padValue = NA)
+        no2_targetyr_smooth <- no2_targetyr %>%
+          focal(focalWeight(., 100, "circle"), mean, na.rm = T, pad = T, padValue = NA)
+
+        no2_ratio <- no2_targetyr_smooth / no2_11_smooth
+
+        if(!is.null(no2_min_incr)) {
+          no2_ratio <- no2_ratio %>% max(no2_min_incr)
+        }
+
+        conc_no2 <- conc_no2 %>% multiply_by(no2_ratio)
       }
 
-      conc_no2 <- conc_no2 %>% multiply_by(no2_ratio)
+      conc_no2[] <- conc_no2[] %>% na.approx(maxgap = 5, na.rm = F)
+      conc_no2
+    } else if(spec == 'so2') {
+      grid_raster %>% setValues(10)
+    } else if(spec == 'tpm10') {
+      get_conc_baseline_pm25(target_year = no2_targetyear, grid_raster = grid_raster) %>%
+        multiply_by(1 / pm25_to_pm10_ratio)
+    } else if(spec == 'pm25'){
+      get_conc_baseline_pm25(target_year = no2_targetyear, grid_raster = grid_raster)
     }
 
-    conc_no2[] <- conc_no2[] %>% na.approx(maxgap=5, na.rm=F)
-    conc[["no2"]] <- conc_no2
-  }
-
-  if("o3" %in% species) {
-    #source of NO2 data: https://data.world/datasets/no2
-    conc[["o3"]] <- creahelpers::get_concentration_path('O3_77e3b7-xmessy_mmd_kk.nc') %>%
-      raster %>%
-      creahelpers::cropProj(grid_raster)
-  }
-
-  if("so2" %in% species) {
-    conc[["so2"]] <- grid_raster %>% `values<-`(10) # why all the same value?
-  }
+  }) %>% `names<-`(species)
 
   tibble(species = names(conc),
          conc_baseline = conc)
@@ -123,7 +120,7 @@ get_conc_baseline <- function(species,
 #' @examples
 get_conc_baseline_pm25 <- function(target_year = lubridate::year(lubridate::today()),
                                    grid_raster){
-  basemap_year <- get_baseline_pm25_year(target_year)
+  basemap_year <- get_baseline_pm25_year(target_year) # get latest available basemap year
 
   pm25_nc <- glue("V5GL03.HybridPM25.Global.{basemap_year}01-{basemap_year}12.nc")
   creahelpers::get_concentration_path(pm25_nc) %>% rast %>%
