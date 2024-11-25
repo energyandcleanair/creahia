38082dadc6a8de2d21bac89b6e133ad237a2f2c3
hubert-crea <hubert@energyandcleanair.org>
2024-11-25 15:18:26 +0100

    -removing many-to-many warning

--- tests/testthat/test_extract_concs_and_pop.R
+++ tests/testthat/test_extract_concs_and_pop.R
@@ -1,3 +1,46 @@
+get_random_exposure_hia <- function(levels, min=20, max=60, target=0, calc_causes = "GEMM and GBD"){
+
+  library(terra)
+  library(creahelpers)
+  library(dplyr)
+  library(creahia)
+  library(creaexposure)
+
+  # Get PM2.5 exposure raster over Bangladesh with resolution 0.01deg
+  res <- 0.01
+  baseline_rast <- terra::rast(
+    xmin=88,
+    xmax=92,
+    ymin=20,
+    ymax=27,
+    res=res,
+    crs="+proj=longlat +datum=WGS84")
+
+  baseline_rast[] <- runif(prod(dim(baseline_rast)), min, max)
+
+  # Build two perturbations:
+  # p1: bring it down to 0
+  # p2: bring it down to WHO2021
+  perturbation_rast <- target-baseline_rast
+
+  # Compute HIAs
+  lapply(levels, function(level){
+    creahia::wrappers.compute_hia_two_images.default(
+      perturbation_rasters = list(pm25 = perturbation_rast),
+      baseline_rasters = list(pm25 = baseline_rast),
+      scale_base_year = NULL, # Just to avoid unnecessary warning
+      scale_target_year = NULL,  # Just to avoid unnecessary warning
+      pop_year=2020,
+      administrative_level = level,
+      administrative_res = "low",
+      administrative_iso3s = "BGD",
+      epi_version = "gbd2019",
+      calc_causes = calc_causes
+    ) %>%
+      mutate(level=level)
+  }) %>%
+    bind_rows()
+}
 
 
 test_that("Regions extraction works and doesn't have any double counting", {
@@ -9,10 +52,27 @@ test_that("Regions extraction works and doesn't have any double counting", {
   library(creahia)
   library(creaexposure)
 
-  res <- creaexposure::RES_30_SEC
-  bbox <- creaexposure::get_bbox("BD")
-  pop <- creaexposure::data.pop(res=res, bbox=bbox)
-  pm25 <- creaexposure::data.basemap_pm25(pop=pop, res=res, year=2019)
+  # Get PM2.5 exposure raster over Bangladesh with resolution 0.01deg
+  res <- 0.01
+  pm25 <- terra::rast(
+    xmin=88,
+    xmax=92,
+    ymin=20,
+    ymax=27,
+    res=res,
+    crs="+proj=longlat +datum=WGS84")
+
+  pm25[] <- runif(prod(dim(pm25)), min, max)
+
+  # Build two perturbations:
+  # p1: bring it down to 0
+  # p2: bring it down to WHO2021
+  # perturbation_rast <- target-pm25
+
+  # res <- creaexposure::RES_30_SEC
+  # bbox <- creaexposure::get_bbox("BD")
+  # pop <- creaexposure::data.pop(res=res, bbox=bbox)
+  # pm25 <- creaexposure::data.basemap_pm25(pop=pop, res=res, year=2019)
   pop_year <- 2020
 
   # Build required data ---------------------------------------------------------------------
@@ -66,28 +126,5 @@ test_that("Regions extraction works and doesn't have any double counting", {
   bind_rows(conc_regions_0$test) %>% bind_rows() %>% summarise(conc=weighted.mean(conc_baseline_pm25, pop, na.rm=T)) %>% pull() -> conc_hia_0
   bind_rows(conc_regions_1$test) %>% bind_rows() %>% summarise(conc=weighted.mean(conc_baseline_pm25, pop, na.rm=T)) %>% pull() -> conc_hia_1
   testthat::expect_equal(conc_hia_0, conc_hia_1, tolerance=1e-2)
-
-  # Compute hia for boths
-  hia_0 <- creahia::wrappers.compute_hia_two_images.default(
-    perturbation_rasters = perturbation_rasters,
-    baseline_rasters = baseline_rasters,
-    administrative_iso3s = c("BGD"),
-    administrative_level = 0,
-    administrative_res = "low",
-    pop_year=2020)
-
-  hia_1 <- creahia::wrappers.compute_hia_two_images.default(
-    perturbation_rasters = perturbation_rasters,
-    baseline_rasters = baseline_rasters,
-    administrative_iso3s = c("BGD"),
-    administrative_level = 1,
-    administrative_res = "low",
-    pop_year=2020
-    )
-
-  # Check deaths are similar
-  hia_0 %>% filter(estimate=="central", Outcome=="Deaths", !double_counted) %>% pull(number) %>% sum() -> deaths_0
-  hia_1 %>% filter(estimate=="central", Outcome=="Deaths", !double_counted) %>% pull(number) %>% sum() -> deaths_1
-  testthat::expect_equal(deaths_0, deaths_1, tolerance=1e-2)
 
 })
