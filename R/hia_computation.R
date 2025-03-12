6b6ee2d3b20b640c443804f802b9af67d9fdbdc9
hubert-crea <hubert@energyandcleanair.org>
2025-03-12 22:19:54 +0100

    -assuming perfect correlation between RRs base and perm

--- R/hia_computation.R
+++ R/hia_computation.R
@@ -473,7 +473,7 @@ country_paf_perm <- function(pm.base,
                                simplify = 'array')
 
 
-    paf <- get_paf_from_rr_delta(
+    paf <- get_paf_from_rr_correlated(
       rr_base = rr.base,
       rr_perm = rr.perm,
       age_weights = age_weights$val,
