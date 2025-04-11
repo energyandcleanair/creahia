91446db724a3708d9a6570bfaf3e68340eaf412e
hubert-crea <hubert@energyandcleanair.org>
2025-04-11 19:53:00 +0100

    -tests improvements though many are still failing due to confidence interval issues

---
+++ tests/testthat/test_retrocompatibility.R
@@ -0,0 +1,60 @@
+library(testthat)
+
+test_that("Results using rr_sources is simlar to old way using calc_causes", {
+
+
+  res <- 0.01
+  m <- terra::rast(
+    xmin=88,
+    xmax=92,
+    ymin=20,
+    ymax=27,
+    res=res,
+    crs="+proj=longlat +datum=WGS84")
+
+  m[] <- 60
+
+  # Build two perturbations:
+  # p1: bring it down to 0
+  # p2: bring it down to WHO2021
+  p1 <- -m
+  p2 <- 5-m
+
+
+  # GEMM and GBD
+  hia_old <- creahia::wrappers.compute_hia_two_images.default(
+      perturbation_rasters = list(pm25 = p1),
+      baseline_rasters = list(pm25 = m),
+      pop_year = 2020,
+      administrative_level = 1,
+      administrative_res = "low",
+      administrative_iso3s = "BGD",
+      calc_causes = 'GEMM and GBD',
+      gbd_version = "gbd2019",
+      gbd_causes = "default"
+    )
+
+  hia_new <- creahia::wrappers.compute_hia_two_images.default(
+    perturbation_rasters = list(pm25 = p1),
+    baseline_rasters = list(pm25 = m),
+    pop_year = 2020,
+    administrative_level = 1,
+    administrative_res = "low",
+    administrative_iso3s = "BGD",
+    rr_source = c(RR_GEMM, RR_ORIGINAL)
+  )
+
+  comparison <- full_join(
+    hia_old %>% rename(number_old=number),
+    hia_new %>% rename(number_new=number)
+  ) %>%
+  # These should differ on LRI.child: in GEMM and GBD, LRI.child is from GBD
+  # However, when we define rr_sources = c(GEMM, ORIGINAL) it will takes GEMM's
+  filter(Cause != 'LRI.child')
+
+  expect_equal(
+    comparison$number_old,
+    comparison$number_new,
+    tolerance = 0.01
+  )
+})
---
+++ tests/testthat/test_rr_sources.R
@@ -0,0 +1,59 @@
+test_that("Results with different RR sources are properly ordered", {
+
+
+
+  res <- 0.01
+  m <- terra::rast(
+    xmin=88,
+    xmax=92,
+    ymin=20,
+    ymax=27,
+    res=res,
+    crs="+proj=longlat +datum=WGS84")
+
+  m[] <- 60
+
+  # Build two perturbations:
+  # p1: bring it down to 0
+  # p2: bring it down to WHO2021
+  p1 <- -m
+  p2 <- 5-m
+
+  ordered_sources <- c(
+    RR_ORIGINAL,
+    RR_GBD2019,
+    RR_GBD2021,
+    RR_GEMM,
+    RR_FUSION # Fusion > old GEMM (the one we have) but lower than new GEMM (e.g. in Fusion paper)
+  )
+
+  hias <- lapply(ordered_sources, function(rr_source) {
+    creahia::wrappers.compute_hia_two_images.default(
+      perturbation_rasters = list(pm25 = p1),
+      baseline_rasters = list(pm25 = m),
+      pop_year = 2020,
+      administrative_level = 1,
+      administrative_res = "low",
+      administrative_iso3s = "BGD",
+      rr_sources = rr_source
+    ) %>%
+      mutate(
+        rr_source = rr_source
+      )
+  }) %>%
+    bind_rows()
+
+
+  ordered_sources_out <- hias %>%
+    filter(estimate=="central", Outcome=="Deaths", !double_counted) %>%
+    group_by(rr_source) %>%
+    summarise(sum=sum(number)) %>%
+    arrange(abs(sum))
+
+
+  expect_equal(
+    ordered_sources_out$rr_source,
+    ordered_sources
+  )
+
+})
