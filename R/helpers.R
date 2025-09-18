35ea0833edc6f16605926408b733bd8cc71a5d14
hubert-crea <hubert@energyandcleanair.org>
2025-09-18 19:15:34 +0100

    -get_model_adm -> get_adm as there was a mix of both. Not to be mistaken with creahelpers::get_adm though

--- R/helpers.R
+++ R/helpers.R
@@ -114,7 +114,7 @@ make_nothing <- function(x) {x}
 #' @export
 #'
 #' @examples
-get_model_adm <- function(grid_raster, shp = NULL,
+get_adm <- function(grid_raster, shp = NULL,
                           admin_level = 0, iso3s = NULL, ...) {
 
   grid_raster <- grid_raster %>% terra::rast()
