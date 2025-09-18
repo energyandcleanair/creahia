252db07e5781440810133114fbd53494e59dc25a
hubert-crea <hubert@energyandcleanair.org>
2025-09-18 18:43:55 +0100

    -explicit joins to avoid unnecessary logs -moved gemm

--- examples/workflow_hia_chile.R
+++ examples/workflow_hia_chile.R
@@ -130,7 +130,6 @@ scenarios <- names(conc_adm)
 # 06: Get HIA data, in case you want to modify them.  ---------------------------------------
 crfs <- get_crfs()
 epi <- get_epi()
-gemm <- get_gemm()
 ihme <- get_ihme()
 gbd_rr <- get_gbd_rr()
 calc_causes <- get_calc_causes()
