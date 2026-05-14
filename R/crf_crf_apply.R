abb43af042bec653361f25f79293f282715335d6
MiaoChien <miao23641@gmail.com>
2026-05-14 17:25:21 +0800

    Create crf_apply.R framework

---
+++ R/crf_crf_apply.R
@@ -0,0 +1,27 @@
+apply_crf_tabular <- function(
+  crf,
+  conc_base,
+  conc_perm,
+  pop,
+  region_id,
+  epi_version = "default"
+){
+  
+  # load crf data
+
+  # get age
+
+  # get age weight
+
+  # calculate rr_base_paf
+
+  # calculate rr_perm_paf
+
+  # use get_paf_from_rr_lauri to calculate age-population weighted paf
+  
+}
+
+
+load_crf_tabular <- function(crf) {
+  
+}
