167cc5762de3f642d3cc3305cea95a5a55903b50
MiaoChien <miao23641@gmail.com>
2026-06-15 12:27:39 +0800

    Update compile_crfs_gemm.R
    Delete duplicate functions

--- R/compile_crfs_gemm.R
+++ R/compile_crfs_gemm.R
@@ -66,42 +66,6 @@ compile_gemm_ihd_25plus_deaths <- function(
   registry_row
 }
 
-upsert_crf_registry_row <- function(registry_row) {
-  # This function takes a single row of CRF registry data (as a tibble) 
-  # and either updates the existing entry in the CRF registry with the same crf_id 
-  # or inserts it if it doesn't exist. It validates the updated registry and saves it back to the CSV file.
-
-  registry_path <- get_hia_path("crf/registry.csv", error_if_not_exists = TRUE)
-
-  registry <- readr::read_csv(registry_path, col_types = readr::cols())
-
-  registry <- registry %>%
-    dplyr::filter(crf_id != registry_row$crf_id) %>%
-    dplyr::bind_rows(registry_row)
-
-  validate_crf_registry(registry)
-
-  readr::write_csv(registry, registry_path)
-
-  invisible(TRUE)
-}
-
-upsert_crf_reference_row <- function(reference_row) {
-  references_path <- get_hia_path("crf/references.csv", error_if_not_exists = TRUE)
-
-  references <- readr::read_csv(references_path, col_types = readr::cols())
-
-  references <- references %>%
-    dplyr::filter(reference_id != reference_row$reference_id) %>%
-    dplyr::bind_rows(reference_row)
-
-  validate_crf_references(references)
-
-  readr::write_csv(references, references_path)
-
-  invisible(TRUE)
-}
-
 validate_compiled_gemm_ihd <- function(tbl) {
   required_cols <- c("exposure", "age", "low", "central", "high")
   missing_cols <- setdiff(required_cols, names(tbl))
